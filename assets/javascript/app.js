5e2e4a94291e7c8b4e116902ca5c2bd12733a2d9
Herman Liu <hermanliu95@gmail.com>
2019-10-04 15:50:26 -0700

    css updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+//Create an array of Words
+const words = ['fat', 'lazy', 'hungry'];
+//Choose words randomly
+let choosenWord = Math.floor(Math.random() * words.length);
+console.log ('choosenWords')
+//Create underscores based on length of word
+//Get users guess
+//check if guess is right
+//if right push to right array
+//if wrong push to wrong array
+
+
